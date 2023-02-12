01e66faac64335e36ef549acf1ccaf2a31ddf1d6
ThunderNL <stijntdm@gmail.com>
2023-02-12 21:35:50 +0100

    Add files via upload

---
+++ Spacy/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.modules.joystick = false
+    t.modules.physics = false
+    
+    
+    t.window.title = "Spacy"
+    t.window.resizable = true
+    t.window.icon = "icon.png"
+end
